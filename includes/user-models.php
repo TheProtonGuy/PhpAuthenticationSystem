7871a2698c3a58405a3c8d00f7c1f5666c70f78a
Ayodeji Adesola <95865361+TheProtonGuy@users.noreply.github.com>
2024-03-31 22:01:05 +0300

    Changed get_user_username to get all user fields

--- includes/user-models.php
+++ includes/user-models.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 function get_user_username(object $pdo, string $username) {
 
-    $query = "SELECT id, username, email FROM users WHERE username = :username;";
+    $query = "SELECT * FROM users WHERE username = :username;";
     $statement = $pdo->prepare($query);
 
     $statement->bindparam(":username", $username);
